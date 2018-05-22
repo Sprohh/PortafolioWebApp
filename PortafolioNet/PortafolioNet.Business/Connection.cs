8d689c61d8fdab95ca9120dc88f3643fa03e493b
DESKTOP-C0V2FF2\Sergio Orellana <orellana123@gmail.com>
2018-05-22 01:52:07 -0400

    Login + Manejadores

---
+++ PortafolioNet/PortafolioNet.Business/Connection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortafolioNet.Business
+{
+    internal class Connection
+    {
+        #region Campos
+        private static LindaSonrisaEntities _lindaSonrisaDB; //Conexión con la DataBase
+        #endregion
+
+        #region Properties
+        public static LindaSonrisaEntities LindaSonrisaDB
+        {
+            get
+            {
+                if (_lindaSonrisaDB == null)
+                    _lindaSonrisaDB = new LindaSonrisaEntities();
+                return _lindaSonrisaDB;
+            }
+            set
+            {
+                _lindaSonrisaDB = value;
+            }
+        }
+        #endregion
+    }
+}
--- PortafolioNet/libreria/Class1.cs
+++ PortafolioNet/libreria/Class1.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PortafolioNet.Business
+namespace libreria
 {
     public class Class1
     {
